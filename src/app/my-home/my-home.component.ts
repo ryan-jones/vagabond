fcc2bc5e3fcbdefe2ad8c40fa887416eb702f359
Ryan Jones <ryanjones@Ryans-MacBook.local>
2017-05-25 13:02:10 +0200

    resolved color coding issues

--- src/app/my-home/my-home.component.ts
+++ src/app/my-home/my-home.component.ts
@@ -33,7 +33,7 @@ export class MyHomeComponent implements OnInit {
 
   ngOnInit() {
     this.initiateMap();
-    google.maps.event.addDomListener(window, 'load', this.initiateMap);
+
     this.country.getList()
       .subscribe((countries) => {
         this.countries = countries;
@@ -61,12 +61,20 @@ export class MyHomeComponent implements OnInit {
 
 //*************** Creates a point on the map **********
   createPoint(){
+
+    // initialize the map
+
+
+
     this.geocoder = new google.maps.Geocoder();
       this.geocodeAddress(this.geocoder, this.map);
   }
 
 //********************* geocoder function *******************
   geocodeAddress(geocoder, resultsMap) {
+    console.log("geo",geocoder)
+     console.log("geresultsMapo",resultsMap)
+      console.log(this.newAddress)
 	  this.address = this.newAddress;
 
 	  geocoder.geocode({'address': this.address}, function(results, status) {
@@ -87,9 +95,6 @@ export class MyHomeComponent implements OnInit {
 
 //********************   creates country data layers ***************
   showCountries(selectedNationalityId1, selectedNationalityId2){
-    this.initiateMap()
-    console.log("selectedNationalityId1", this.selectedNationalityId1);
-    console.log("selectedNationalityId2", this.selectedNationalityId2);
 
     this.country.get(this.selectedNationalityId1)
         .subscribe((nation) => {
@@ -103,14 +108,39 @@ export class MyHomeComponent implements OnInit {
 
           for( var i=0; i<= this.nation.visaFree.length; i++){
             freeLayer.loadGeoJson('https://raw.githubusercontent.com/johan/world.geo.json/master/countries/' + this.nation.visaFree[i] + '.geo.json');
-            freeLayer.setStyle({ fillColor: 'yellow', fillOpacity: 0.5});
+            console.log("visaFree",this.nation.visaFree[i])
+            freeLayer.setStyle({ fillColor: 'blue', fillOpacity: 0.3});
           }
 
           for(var j=0; j<= this.nation.visaOnArrival.length; j++){
             onArrivalLayer.loadGeoJson('https://raw.githubusercontent.com/johan/world.geo.json/master/countries/' + this.nation.visaOnArrival[j] + '.geo.json');
+            console.log("visaOnArrival", this.nation.visaOnArrival[j])
 
-            onArrivalLayer.setStyle({ fillColor: 'red',  fillOpacity: 0.5});
+            onArrivalLayer.setStyle({ fillColor: 'yellow',  fillOpacity: 0.3});
           }
+
+
+          this.country.get(this.selectedNationalityId2)
+              .subscribe((nation2) => {
+                this.nation2 = nation2;
+                this.countryName2 = this.nation2;
+                var onArrivalLayer2 = new google.maps.Data();
+                var freeLayer2 = new google.maps.Data();
+                var banLayer2 = new google.maps.Data();
+
+
+                for( var i=0; i<= this.nation2.visaFree.length; i++){
+                  freeLayer2.loadGeoJson('https://raw.githubusercontent.com/johan/world.geo.json/master/countries/' + this.nation2.visaFree[i] + '.geo.json');
+                      console.log("visaFree2",this.nation.visaFree[i])
+                  freeLayer2.setStyle({ fillColor: 'red', fillOpacity: 0.8});
+                }
+
+                for(var j=0; j<= this.nation2.visaOnArrival.length; j++){
+                  onArrivalLayer2.loadGeoJson('https://raw.githubusercontent.com/johan/world.geo.json/master/countries/' + this.nation2.visaOnArrival[j] + '.geo.json');
+    console.log("visaOnArrival2", this.nation.visaOnArrival[j])
+                  onArrivalLayer2.setStyle({ fillColor: 'green',  fillOpacity: 0.8});
+                }
+
 
           // for (var f = 0; f <=this.nation.bannedFrom.length; f++){
           //   banLayer.loadGeoJson('https://raw.githubusercontent.com/johan/world.geo.json/master/countries/' + this.nation.bannedFrom[i] + '.geo.json')
@@ -122,50 +152,55 @@ export class MyHomeComponent implements OnInit {
           freeLayer.setMap(this.map);
           banLayer.setMap(this.map);
 
+          onArrivalLayer2.setMap(this.map);
+          freeLayer2.setMap(this.map);
+          banLayer2.setMap(this.map);
 
           });
-          this.showCountries2(selectedNationalityId2);
-      } //showCountries
+          // this.showCountries2(selectedNationalityId2);
+      }) //showCountries
 
 
 
 //****************  Second nation selector *****************
 
-      showCountries2(selectedNationalityId2){
-        this.country.get(this.selectedNationalityId2)
-            .subscribe((nation) => {
-              this.nation = nation;
-              this.countryName2 = this.nation;
-              console.log('in subscribe', this.nation2);
-              var onArrivalLayer2 = new google.maps.Data();
-              var freeLayer2 = new google.maps.Data();
-              var banLayer2 = new google.maps.Data();
+      // showCountries2(selectedNationalityId2){
+      //   this.country.get(this.selectedNationalityId2)
+      //       .subscribe((nation) => {
+      //         this.nation = nation;
+      //         this.countryName2 = this.nation;
+      //         console.log('in subscribe', this.nation2);
+      //         var onArrivalLayer2 = new google.maps.Data();
+      //         var freeLayer2 = new google.maps.Data();
+      //         var banLayer2 = new google.maps.Data();
+      //
+      //
+      //         //
+      //         // for( var i=0; i<= this.nation.visaFree.length; i++){
+      //         //   freeLayer2.loadGeoJson('https://raw.githubusercontent.com/johan/world.geo.json/master/countries/' + this.nation.visaFree[i] + '.geo.json');
+      //         //   freeLayer2.setStyle({ fillColor: 'blue', fillOpacity: 0.5});
+      //         // }
+      //         //
+      //         // for(var j=0; j<= this.nation.visaOnArrival.length; j++){
+      //         //   onArrivalLayer2.loadGeoJson('https://raw.githubusercontent.com/johan/world.geo.json/master/countries/' + this.nation.visaOnArrival[j] + '.geo.json');
+      //         //   onArrivalLayer2.setStyle({ fillColor: 'green', fillOpacity: 0.5});
+      //         // }
+      //         //
+      //
+      //
+      //         // for (var f = 0; f <=this.nation.bannedFrom.length; f++){
+      //         //   banLayer2.loadGeoJson('https://raw.githubusercontent.com/johan/world.geo.json/master/countries/' + this.nation.bannedFrom[i] + '.geo.json')
+      //         //   banLayer2.setStyle({ fillColor: 'black', fillOpacity: 1});
+      //         // }
+      //
+      //         onArrivalLayer2.setMap(this.map);
+      //         freeLayer2.setMap(this.map);
+      //         banLayer2.setMap(this.map);
+      //
+      //
+      //         });
+      //
+      //     } //showCountries
+}
 
-
-
-              for( var i=0; i<= this.nation.visaFree.length; i++){
-                freeLayer2.loadGeoJson('https://raw.githubusercontent.com/johan/world.geo.json/master/countries/' + this.nation.visaFree[i] + '.geo.json');
-                freeLayer2.setStyle({ fillColor: 'blue', fillOpacity: 0.5});
-              }
-
-              for(var j=0; j<= this.nation.visaOnArrival.length; j++){
-                onArrivalLayer2.loadGeoJson('https://raw.githubusercontent.com/johan/world.geo.json/master/countries/' + this.nation.visaOnArrival[j] + '.geo.json');
-                onArrivalLayer2.setStyle({ fillColor: 'green', fillOpacity: 0.5});
-              }
-
-
-
-              // for (var f = 0; f <=this.nation.bannedFrom.length; f++){
-              //   banLayer2.loadGeoJson('https://raw.githubusercontent.com/johan/world.geo.json/master/countries/' + this.nation.bannedFrom[i] + '.geo.json')
-              //   banLayer2.setStyle({ fillColor: 'black', fillOpacity: 1});
-              // }
-
-              onArrivalLayer2.setMap(this.map);
-              freeLayer2.setMap(this.map);
-              banLayer2.setMap(this.map);
-
-
-              });
-
-          } //showCountries
 }
